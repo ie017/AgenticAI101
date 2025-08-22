9ba8f18d998b8924c9f2a10e23f09132c014c874
ISSAM EL ORF <issam.el-orf@inetum.com>
2025-08-22 23:21:21 +0100

    learn: create first controller based on ChatClient interface,

--- src/main/java/com/ie017/agenticai/agenticai/controller/AgenticAIController.java
+++ src/main/java/com/ie017/agenticai/agenticai/controller/AgenticAIController.java
@@ -1,9 +1,12 @@
 package com.ie017.agenticai.agenticai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/agenticai")
 public class AgenticAIController {
     // Interface qui vous permet de communiquer avec n'import quel LLM (openai, ollama, ...)
     private ChatClient chatClient;
@@ -12,6 +15,14 @@ public class AgenticAIController {
         // Avec le pattern builder on peut ajouter des configurations a notre interface chatClient
         this.chatClient = builder
                 .build();
+    }
+
+    @GetMapping("/chat")
+    public String askLLm(String query) {
+        return chatClient.prompt()// prompt utiliser pour la creation d'un prompt avec ces composants (system message, user message, exemple message)
+                .user(query) // user message
+                .call() // Pour avoir la reponse
+                .content(); // retourner le resultat de la reponse
     }
 
 
