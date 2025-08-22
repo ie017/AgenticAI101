de26e3b7c6453e762c127cc918a9a4fcc5a4b03d
ISSAM EL ORF <issam.el-orf@inetum.com>
2025-08-23 00:13:59 +0100

    learn: From ZeroShotPrompting to FewShotPrompting,

--- src/main/java/com/ie017/agenticai/agenticai/controller/AgenticAIController.java
+++ src/main/java/com/ie017/agenticai/agenticai/controller/AgenticAIController.java
@@ -4,9 +4,14 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/agenticai")
@@ -24,14 +29,28 @@ public class AgenticAIController {
                 .build();
     }
 
-    @GetMapping("/chat")
-    public String askLLm(String query) {
+    @GetMapping("/ZeroShotPromptingChat")
+    public String askLLmBasedOnZeroShotPromptingChat(String query) {
         return chatClient.prompt()// prompt utiliser pour la creation d'un prompt avec ces composants (system message, user message, exemple message)
                 // Ensemble des instructions que vous donnez dans le prompt, et qui le LLM doit respecter pour repondre a la query (question)
                 .system("Use uppercase in every response, dont forget you are a software developer")
                 .user(query) // user message
                 .call() // Pour avoir la reponse
                 .content(); // retourner le resultat de la reponse
+    }
+
+    @GetMapping("/FewShotPromptingChat")
+    public String askLLmBasedOnFewShotPromptingChat(String query) {
+        List<Message> examples = List.of(
+                new UserMessage("what is the github account of ie017?"),
+                new AssistantMessage("Voila take this link : https://github.com/ie017")
+        );
+        return chatClient.prompt()
+                .system("Use uppercase in every response, dont forget you are a software developer")
+                .messages(examples)
+                .user(query)
+                .call()
+                .content();
     }
 
 
