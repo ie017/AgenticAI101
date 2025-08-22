c9b2cdc68e33bb547b6a8cba118d635187aeaadf
ISSAM EL ORF <issam.el-orf@inetum.com>
2025-08-22 23:58:07 +0100

    learn: Configure Advisor to add memory to our agent ai,

--- src/main/java/com/ie017/agenticai/agenticai/controller/AgenticAIController.java
+++ src/main/java/com/ie017/agenticai/agenticai/controller/AgenticAIController.java
@@ -1,7 +1,9 @@
 package com.ie017.agenticai.agenticai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,13 @@ public class AgenticAIController {
     // Interface qui vous permet de communiquer avec n'import quel LLM (openai, ollama, ...)
     private ChatClient chatClient;
 
-    public AgenticAIController(ChatClient.Builder builder) {
+    public AgenticAIController(ChatClient.Builder builder, ChatMemory chatMemory) {
         // Avec le pattern builder on peut ajouter des configurations a notre interface chatClient
         this.chatClient = builder
-                // Je demmande a utiliser c'intercepteur a chaque fois le client envoi un requete
+                // Je demmande a utiliser ces'intercepteurs a chaque fois le client envoi une requete
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                //  ChatMemory est l'interface qui nous donne la mémoire par défaut de Spring AI avec un max des messages = 20, ce qui nous permet de régler le comportement de notre agent IA (agent ai are stateless by default)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
     }
 
