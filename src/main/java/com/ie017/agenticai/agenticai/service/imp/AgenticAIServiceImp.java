a5abdc7154fa59a70bf8801b4f3a167c24bb9eb7
ie017 <elorfissam@gmail.com>
2025-09-06 15:09:42 +0100

    learn: Add system RAG as a tool,

--- src/main/java/com/ie017/agenticai/agenticai/service/imp/AgenticAIServiceImp.java
+++ src/main/java/com/ie017/agenticai/agenticai/service/imp/AgenticAIServiceImp.java
@@ -8,6 +8,7 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -17,15 +18,19 @@ public class AgenticAIServiceImp implements AgenticAIService {
 
     private ChatClient chatClient;
 
-    public AgenticAIServiceImp(ChatClient.Builder chatClient, ChatMemory chatMemory, AgentTools agentTools,
-                               SimpleVectorStore simpleVectorStore) {
+
+    /** To work with System RAG (Retrieval Augmented Generation) as a tool we need to disable it in the constructor
+     * and late defaultTools in the constructor because the tools need to be scanned after the agent is created
+     * */
+    public AgenticAIServiceImp(ChatClient.Builder chatClient, ChatMemory chatMemory, AgentTools agentTools/*,
+                               VectorStore simpleVectorStore*/) {
         this.chatClient = chatClient
                 // Pour avoir une mémoire de conversation
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 // Pour associer notre agent avec les outils, après cette étape, notre agent est capable de scanner toutes les méthodes qui sont annotées avec @Tools pour récupérer les informations
                 .defaultTools(agentTools)
-                // Pour activer la capacité de répondre aux questions en fonction du contexte fourni par le vector store
-                .defaultAdvisors(new QuestionAnswerAdvisor(simpleVectorStore))
+                // Pour activer la capacité de répondre aux questions en fonction du contexte fourni par le vector store : System RAG ( Retrieval Augmented Generation)
+                //.defaultAdvisors(new QuestionAnswerAdvisor(simpleVectorStore))
                 // Pour logger toutes les requetes et les réponses
                 .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
